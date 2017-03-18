d239024eaa8a7b79fba5d2f4be8f1ce93041e742
ARCosades <alexjrobson@gmail.com>
2017-03-18 00:09:46 +0000

    In the middle of converting from the semi supoervised action labell approach to the (est. value, actual value) approach

--- Cartpole/cartpole.py
+++ Cartpole/cartpole.py
@@ -33,90 +33,91 @@ def PolicyNetwork(input_var):
                           W=GlorotNormal(gain=1))
                )
     network = DenseLayer(incoming=network,
-                                        num_units=1,
+                                        num_units=2,
                                         W=GlorotNormal(),
-                                        nonlinearity=sigmoid)
+                                        nonlinearity=rectify)
+    network = lasagne.layers.ReshapeLayer(network, (2,))
     return network
 
 
-def RunEpisode(env, choose_action, creward, memory):
+def RunEpisode(env, get_preferences, choose_action):
 
+    expected_reward = []
+    creward = []
+    observations = []
     obs = env.reset()
     for t in range(1000):
-        memory['obs'].append(obs)
-        action = choose_action(obs.astype('float32').reshape(1, 4), random_sampler())
-        memory['act'].append(action[0][0])
-        obs, reward, done, info = env.step(action[0][0])
-        creward += reward
+        #Assess options
+        action_reward = get_preferences(obs.astype('float32').reshape(1,4))
+        # Decide
+        action = choose_action(obs.astype('float32').reshape(1, 4))
+        expected_reward.append(action_reward)
+        # Take action and observe
+        obs, reward, done, info = env.step(action)
+        creward.append(reward)
+        observations.append(obs)
+
         if done:
             break
 
-    return creward, memory
+    return observations, creward, expected_reward
 
-def trainmodel(choose_action, random_sampler, D_train, D_params):
+def trainmodel(get_preferences, choose_action, D_train, D_params):
 
-	Rgoal = 100
-	eps_per_update = 1
-	Rtol = 195
-	req_number = 100
-	env = gym.make('CartPole-v0')
-	env.reset()
-	lossplot = []
-	rewardplot = []
-	weightplot = []
-	bestreward = 0
-	running_score = []
-	N = 0
-	needs_more_training = True
-	rescale = 1
-	while needs_more_training:
-		N += 1
-		if N % 100 == 0:
-			print("Running {}th update".format(N))
+    Rgoal = 100
+    eps_per_update = 1
+    Rtol = 195
+    req_number = 100
+    env = gym.make('CartPole-v0')
+    env.reset()
+    lossplot = []
+    rewardplot = []
+    weightplot = []
+    bestreward = 0
+    running_score = []
+    N = 0
+    needs_more_training = True
+    while needs_more_training:
+        N += 1
+        if N % 100 == 0:
+            print("Running {}th update".format(N))
 
-		memory = {}
-		memory['obs'] = []
-		memory['act'] = []
-		creward = 0.
+        for _ in range(eps_per_update):
+            observations, actual_reward, expected_reward = RunEpisode(env, get_preferences, choose_action)
+            creward = np.sum(actual_reward)
+            rewardplot.append(creward)
+            running_score.append(creward)
+            if len(running_score)>req_number:
+                running_score.pop(0)
+                print(np.mean(running_score))
+                if np.mean(running_score)>Rtol:
+                    needs_more_training=False
 
-		for _ in range(eps_per_update):
-			creward, memory = RunEpisode(env, choose_action, creward, memory)
-			rewardplot.append(creward)
-			running_score.append(creward)
-			if len(running_score)>req_number:
-				running_score.pop(0)
-				print(np.mean(running_score))
-				if np.mean(running_score)>Rtol:
-					needs_more_training=False
+            if creward / eps_per_update > bestreward:
+                bestreward = creward / eps_per_update
 
-			if creward / eps_per_update > bestreward:
-				bestreward = creward / eps_per_update
-				rescale = 1
-			else:
-				scale = 1
+        lossplot.append(
+            D_train(np.array(observations).astype('float32'),
+                    np.array(actual_reward).astype('int8')))
 
-		lossplot.append(
-			D_train(np.array(memory['obs']).astype('float32'),
-			        np.array(memory['act']).astype('int8')))
-
-		weightplot.append(np.median(D_params[1].get_value()))
+        weightplot.append(np.median(D_params[1].get_value()))
 
 
-def runmodel(choose_action, random_sampler, number_of_episodes=1, monitor=False):
+def runmodel(choose_action, number_of_episodes=1, monitor=False):
 
     env = gym.make('CartPole-v0')
     if monitor:
         env = wrappers.Monitor(env, '/tmp/cartpole-experiment-1')
     env.reset()
     for i_ep in range(number_of_episodes):
-		obs = env.reset()
-		for t in range(1000):
-			env.render()
-			action = choose_action(obs.astype('float32').reshape(1, 4), random_sampler())
-			obs, reward, done, info = env.step(action[0][0])
-			if done:
-				print("Episode finished after {} timesteps".format(t+1))
-				break
+        obs = env.reset()
+        for t in range(1000):
+            env.render()
+            action = choose_action(obs.astype('float32').reshape(1, 4))
+            obs, reward, done, info = env.step(action[0][0])
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
 
 
 def prepare_functions():
@@ -127,50 +128,53 @@ def prepare_functions():
     action_var = T.vector('actions')
     srng = RandomStreams(seed=42)
     Rgoal = T.vector('goal')
+    expected_reward = T.vector('expected')
+    actual_reward = T.vector('actual')
 
     D_network = PolicyNetwork(observations)
     D_params = lasagne.layers.get_all_params(D_network, trainable=True)
 
-    P_act = lasagne.layers.get_output(D_network)
-    f_act = theano.function([observations], P_act)
+    expected_reward = lasagne.layers.get_output(D_network)
+    get_preferences = theano.function([observations], expected_reward)
 
-    # Set up an objective function that uses fake action labels.
-    # This creates a vector of 'correct actions' (action taken when a game is won)
-    D_obj = T.switch(T.gt(reward_var, Rgoal),
-                     lasagne.objectives.binary_crossentropy(P_act, action_var),
-                     lasagne.objectives.binary_crossentropy(P_act, 1-action_var)
-                     ).mean()
+    # Convert from a loss aversion function to a reward seeking function.
+    # This is the sum of the reward accumulated by each action. The loss function
+    # is given by the expected reward minus the actual reward.
 
-    D_obj = -lasagne.objectives.binary_crossentropy(P_act, action_var).mean()
+    # The expected_reward for action is the sum of all rewards subsequent to that action
+    # The actual_reward for the action is the total reward of the episode
+    D_obj = lasagne.objectives.squared_error(expected_reward, actual_reward).mean()
 
     D_updates = lasagne.updates.adam(D_obj, D_params,learning_rate=2e-4)
-    D_train = theano.function([observations, action_var], D_obj, updates=D_updates, name='D_training')
+    D_train = theano.function([observations, actual_reward], D_obj, updates=D_updates, name='D_training')
 
     rv_u = srng.uniform(size=(1,))
-    random_sampler = theano.function([], rv_u)
-    D_out = T.switch(T.lt(lasagne.layers.get_output(D_network), random_var), int(0) ,int(1))
+#    random_sampler = theano.function([], rv_u)
+    action_reward = T.switch(T.lt(expected_reward[0], expected_reward[1]), expected_reward[0], expected_reward[1])
+    get_action_reward = theano.function([observations], action_reward)
+    D_out = T.switch(T.lt(expected_reward[0], expected_reward[1]), int(0) ,int(1))
 
-    choose_action = theano.function([observations, random_var], D_out, name='weighted_choice')
+    choose_action = theano.function([observations], D_out, name='greedy_choice')
 
-    return choose_action, random_sampler, D_train, D_params, D_network
+    return get_action_reward, choose_action, D_train, D_params, D_network
 
 
 def savemodel(network, filename):
     np.savez(filename, *lasagne.layers.get_all_param_values(network))
 
 def initmodel(network, filename):
-	with np.load(filename) as f:
-		param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+    with np.load(filename) as f:
+        param_values = [f['arr_%d' % i] for i in range(len(f.files))]
 
-	lasagne.layers.set_all_param_values(network, param_values)
+    lasagne.layers.set_all_param_values(network, param_values)
 
 
 if __name__=='__main__':
-    choose_action, random_sampler, D_train, D_params, D_network = prepare_functions()
+    get_preferences, choose_action, D_train, D_params, D_network = prepare_functions()
     if True:
-        trainmodel(choose_action, random_sampler, D_train, D_params)
+        trainmodel(get_preferences, choose_action, D_train, D_params)
         savemodel(D_network, 'D_network.npz')
     else:
         initmodel(D_network, 'D_network.npz')
-        runmodel(choose_action, random_sampler)
+        runmodel(choose_action)
 
